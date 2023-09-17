c956d240a981a98af5129e22d1fbdaa31f6abc54
naviafra <navid.afradi@gmail.com>
2023-09-17 14:11:51 +0200

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Installing Docker GPG Key"
+
+apt-get install ca-certificates curl gnupg -y
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo "Add the APT Repository"
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+
+echo "Install Docker"
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+echo "Add user to the Docker Group"
+usermod -aG docker $USER
+
+echo "Install Portainer"
+docker volume create portainer_data
+
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+echo "Installation of Docker and Portainer is finished"
